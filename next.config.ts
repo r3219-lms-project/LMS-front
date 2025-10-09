da7ce0f7937ee3f7d4ddb211f8a92ce7d7a11ae4
undndnwnkk <bykov.lev74@gmail.com>
2025-10-09 13:07:29 +0300

    LMS-44: addded registration screen + logic + endpoint

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  async rewrites() {
+    return [
+      {source: '/screens/:path', destination: "/:path"}
+    ];
+  }
 };
 
 export default nextConfig;
